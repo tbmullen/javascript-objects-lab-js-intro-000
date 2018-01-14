199f45635a429d84f6717264f5022552c6f2c324
Trevor Mullen <tbhackworth@gmail.com>
2018-01-14 16:13:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var recipes = {
 };
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newRecipe = Object.assign({}, object, key: value);
+  var newRecipe = Object.assign({}, object,  { [key]: value) };
   return newRecipe;
 }
