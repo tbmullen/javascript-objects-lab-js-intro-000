2228b544c7e33ba09b3c66e9401fd86c7c4f0557
Trevor Mullen <tbhackworth@gmail.com>
2018-01-14 16:19:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, obj);
+  delete newObj.key;
+  return newObj;
+}
